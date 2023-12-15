013d7a3893015c5d4d4dfa5b100b3ef4f906d23c
Andy Cai <huayicai@gmail.com>
2023-12-16 01:15:08 +0800

    Added admin component match

---
+++ administrator/components/match/handler.go
@@ -0,0 +1,11 @@
+package match
+
+import "github.com/gofiber/fiber/v2"
+
+func LoginPage(c *fiber.Ctx) error {
+	return nil
+}
+
+func LogoutAction(c *fiber.Ctx) error {
+	return nil
+}
---
+++ administrator/components/match/init.go
@@ -0,0 +1,36 @@
+package match
+
+import (
+	"github.com/andycai/axe-fiber/core"
+	"github.com/gofiber/fiber/v2"
+	"gorm.io/gorm"
+)
+
+const (
+	KeyDB            = "admin.match.gorm.db"
+	KeyNoCheckRouter = "admin.match.router.nocheck"
+	KeyCheckRouter   = "admin.match.router.check"
+)
+
+var db *gorm.DB
+
+func initDB(dbs []*gorm.DB) {
+	db = dbs[0]
+}
+
+func initNoCheckRouter(r fiber.Router) {
+	admin := r.Group("/admin")
+	{
+		admin.Get("/login", LoginPage)
+	}
+}
+
+func initCheckRouter(r fiber.Router) {
+	r.Get("/logout", LogoutAction)
+}
+
+func init() {
+	core.RegisterDatabase(KeyDB, initDB)
+	core.RegisterNoCheckRouter(KeyNoCheckRouter, initNoCheckRouter)
+	core.RegisterAdminCheckRouter(KeyCheckRouter, initCheckRouter)
+}
